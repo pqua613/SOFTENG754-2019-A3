909ddb8362bafb3271b1186ac286f80bddb8ae61
Phillip Quach <pqua613@aucklanduni.ac.nz>
2019-04-06 17:37:43 +1300

    Add failing test case

---
+++ src/test/java/nz/ac/auckland/SOFTENG754_2019_A3/DollarTest.java
@@ -0,0 +1,34 @@
+package nz.ac.auckland.SOFTENG754_2019_A3;
+
+import static org.junit.Assert.*;
+
+import org.bson.Document;
+import org.junit.Test;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class DollarTest {
+
+    @Test
+    public void testDollarDivision() {
+
+        // Given
+        Dollar ten = new Dollar(10);
+
+        // When
+        Dollar result = ten.dividedBy(2);
+
+        // Then
+        assertEquals(new Dollar(4), result); // You could change 5 to 4 to see the build failure
+    }
+
+    @Test(expected=ArithmeticException.class)
+    public void testDollarDividedByZero() {
+        // Given
+        Dollar ten = new Dollar(10);
+
+        // When
+        ten.dividedBy(0);
+    }
+}
